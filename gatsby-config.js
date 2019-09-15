0ea24a351f5ae92e5934ac819e6adb2a48b48b58
Petr Messner <petr.messner@gmail.com>
2019-09-15 22:04:25 +0200

    update offline config

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,11 +33,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: ['/', '/2019/*'],
-      },
-    },
+    'gatsby-plugin-offline',
   ],
 }
